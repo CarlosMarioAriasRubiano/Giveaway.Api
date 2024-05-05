ebbcc18125ecee082746a672afcb00f2ce982b3d
LAPTOP-59NB9RSQ\MARIO <marioarias1989@gmail.com>
2024-05-05 10:23:53 -0500

    Se agrega el proyecto de pruebas unitarias con el servicio de usuario

---
+++ Giveaway.Domain.Tests/DataBuilder/UserBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Giveaway.Domain.Entities;
+
+namespace Giveaway.Domain.Tests.DataBuilder
+{
+    public class UserBuilder
+    {
+        private Guid Id;
+        private string Name;
+        private string Identification;
+        private string Email;
+
+        public UserBuilder()
+        {
+            Id = Guid.NewGuid();
+            Name = "Test";
+            Identification = "123456";
+            Email = "test@test.com";
+        }
+
+        public User Build()
+        {
+            return new()
+            {
+                Id = Id,
+                Name = Name,
+                Identification = Identification,
+                Email = Email
+            };
+        }
+
+        public UserBuilder WithId(Guid id)
+        {
+            Id = id;
+            return this;
+        }
+
+        public UserBuilder WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        public UserBuilder WithIdentification(string identification)
+        {
+            Identification = identification;
+            return this;
+        }
+
+        public UserBuilder WithEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+    }
+}
---
+++ Giveaway.Domain.Tests/ServiceTest/UserServiceTest.cs
@@ -0,0 +1,83 @@
+﻿using Giveaway.Domain.Entities;
+using Giveaway.Domain.Exceptions;
+using Giveaway.Domain.Ports;
+using Giveaway.Domain.Services;
+using Giveaway.Domain.Tests.DataBuilder;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using System.Linq.Expressions;
+
+namespace Giveaway.Domain.Tests.ServiceTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        private IGenericRepository<User> UserRepository { get; set; } = default!;
+        private UserService UserService { get; set; } = default!;
+        private UserBuilder UserBuilder { get; set; } = default!;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            UserRepository = Substitute.For<IGenericRepository<User>>();
+
+            UserService = new(
+                UserRepository
+            );
+
+            UserBuilder = new();
+        }
+
+        [TestMethod]
+        public async Task GetUserByIdAsync_Error_NotExists()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            UserRepository
+                .GetByAlternateKeyAsync(user => user.Id == id)
+                .ReturnsNullForAnyArgs();
+
+            // Act
+            ValidatorException ex = await Assert.ThrowsExceptionAsync<ValidatorException>(async () =>
+            {
+                await UserService.GetUserByIdAsync(id);
+            });
+
+            // Assert
+            Assert.AreEqual(MessagesExceptions.UserNotExists, ex.Message);
+            await UserRepository.ReceivedWithAnyArgs(1)
+                .GetByAlternateKeyAsync(
+                    Arg.Any<Expression<Func<User, bool>>>()
+                );
+        }
+
+        [TestMethod]
+        public async Task GetUserByIdAsync_Ok()
+        {
+            // Arrange
+            User user = UserBuilder
+                .WithId(new("2b9bdcf2-f8c9-428f-842f-6dcc9e402e70"))
+                .WithName("Nombre para prueba unitaria")
+                .WithIdentification("55555")
+                .WithEmail("carlos@gmail.com")
+                .Build();
+
+            UserRepository
+                .GetByAlternateKeyAsync(userRepo => userRepo.Id == user.Id)
+                .ReturnsForAnyArgs(user);
+
+            // Act
+            User result = await UserService
+                .GetUserByIdAsync(user.Id);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(user, result);
+            await UserRepository.ReceivedWithAnyArgs(1)
+                .GetByAlternateKeyAsync(
+                    Arg.Any<Expression<Func<User, bool>>>()
+                );
+        }
+    }
+}
